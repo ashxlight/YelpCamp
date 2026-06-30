6fbce40bc0ef2857cbf85f8c6a6c1fc9766589fd
Ashmit Kesharvani <abhishekesharvani9@gmail.com>
2026-06-30 16:10:53 +0530

    Add debug logging

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,8 @@
+const campground = require('../models/campground');
 const Campground = require('../models/campground');
+const {cloudinary}=require("../Cloudinary");
+const maptilerClient = require("@maptiler/client");
+maptilerClient.config.apiKey = process.env.MAPTILER_API_KEY;
 
 
 module.exports.index=async (req, res) => {
@@ -11,9 +15,24 @@ module.exports.renderNewForm=(req, res) => {
 }
 
 module.exports.createCampground=async (req, res,next) => {//new Campground
+    console.log('FILES:', req.files);
+    console.log('MAPTILER KEY:', process.env.MAPTILER_API_KEY ? 'SET' : 'NOT SET');
+
+    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
+    console.log('GEODATA FEATURES:', geoData.features?.length);
+    // console.log(geoData);
+    if (!geoData.features?.length) {
+        req.flash('error', 'Could not geocode that location. Please try again and enter a valid location.');
+        return res.redirect('/campgrounds/new');
+    }
     
     const campground = new Campground(req.body.campground);
-    campground.images= req.files.map(f=>({url:f.path,filename:f.filename}));//will add url and filename in campground->images Schema
+
+    campground.geometry = geoData.features[0].geometry;
+    campground.location = geoData.features[0].place_name;
+
+
+    campground.image= req.files.map(f=>({url:f.path,filename:f.filename}));//will add url and filename in campground->images Schema
     campground.author=req.user._id;
 
     await campground.save();
@@ -29,6 +48,7 @@ module.exports.showCampground=async (req, res) => {//edit page
         populate:{
             path:'author'
         }  
+
     
 }).populate('author');
     if(!campground)
@@ -37,6 +57,8 @@ module.exports.showCampground=async (req, res) => {//edit page
         return res.redirect('/campgrounds');
 
     }
+
+    console.log(campground.images);
 
     res.render('campgrounds/show', { campground });
 }
@@ -49,8 +71,35 @@ module.exports.renderEditForm=async (req, res) => {
 
 module.exports.updateCampground=async (req, res) => {
     const { id } = req.params;
+
+      const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
+    // console.log(geoData);
+    if (!geoData.features?.length) {
+        req.flash('error', 'Could not geocode that location. Please try again and enter a valid location.');
+        return res.redirect(`/campgrounds/${id}/edit`);
+    }
+
+
     const campgroundData = req.body.campground;
-    const camp=await Campground.findByIdAndUpdate(id, campgroundData, { runValidators: true });
+    const campground=await Campground.findByIdAndUpdate(id, campgroundData, { runValidators: true });
+
+    campground.geometry = geoData.features[0].geometry;
+    campground.location = geoData.features[0].place_name;
+
+
+    const imgs=req.files.map(f=>({url:f.path,filename:f.filename}));
+    campground.image.push(...imgs);
+    await campground.save();
+
+    if(req.body.deleteImages)
+    {   
+     for(let filename of req.body.deleteImages)
+     {
+       await cloudinary.uploader.destroy(filename);
+     }
+     await campground.updateOne({$pull:{image:{filename:{$in:req.body.deleteImages}}}});
+    }
+    
     req.flash('success','Successfully updated campground')
     res.redirect(`/campgrounds/${id}`);
 }
